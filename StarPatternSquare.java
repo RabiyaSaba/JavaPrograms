201054d7e1cdf84d877754a0daea4287162c23ab
Rabiya Saba <96332451+RabiyaSaba@users.noreply.github.com>
2023-09-28 13:25:05 +0530

    Add files via upload

---
+++ StarPatternSquare.java
@@ -0,0 +1,20 @@
+// wap a program to print star patter square
+public class StarPatternSquare {
+	
+	public static void main(String args[])
+	{
+		// this will be our length
+		int n =5;
+		
+		for (int i =0;i<n;i++)
+		{
+			for(int j =0;j<n;j++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
