31a152c54ed9de005ac595f91ad1fc93e5c9b432
Rabiya Saba <96332451+RabiyaSaba@users.noreply.github.com>
2023-09-28 13:28:45 +0530

    Add files via upload

---
+++ PositiveOrNegetive.java
@@ -0,0 +1,19 @@
+// wap to check whether the number is positive or negetive
+public class PositiveOrNegetive {
+	
+	public static void main(String args[])
+	{
+		// number to be checked 
+		int number=10;
+		// logic 
+		if(number <0)
+		{
+			System.out.println("Negetive number ");
+		}
+		else
+		{
+			System.out.println("Positive number ");
+		}
+	}
+
+}
